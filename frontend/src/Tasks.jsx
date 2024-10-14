f056fdb9ec5794b68e0ccb429d2118b84241afda
Anup D <dhakalanup96@gmail.com>
2024-10-14 22:44:44 +0545

    trying to iterate tasks in frontend but yet

--- frontend/src/Tasks.jsx
+++ frontend/src/Tasks.jsx
@@ -3,20 +3,24 @@ import axios from 'axios'
 
 const Tasks = () => {
 
-  const [data, setData] = useState('')
+  const [data, setData] = useState([])
 
   const getTasks = async () => {
     const response = await axios.get("http://localhost:3669")
     console.log(response);
-    setData(response?.data[1]?.task)
+    setData(response?.data?.task)
   }
 
   useEffect(()=> {
     getTasks()
   },[])
+
   return (
-      <div className='border border-gray-300 bg-gray-100 rounded-md shadow-xl w-1/3 mx-auto mt-4 text-2xl px-3 py-2 '>{data}</div>
- 
+    <>
+    {data.map(item => (
+      <div key={item._id} className='border border-gray-300 bg-gray-100 rounded-md shadow-xl w-1/3 mx-auto mt-4 text-2xl px-3 py-2 '>{item.task}</div>
+    ))}
+    </>
   )
 }
 
